d8e7f43efd7585b0f94be7e3111bc99bc3d82bbf
Bruno Simm <56409507+BrunoSimm@users.noreply.github.com>
2020-10-30 17:35:33 -0300

    form

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -10,6 +10,12 @@ app.post('/usuarios',(req,resp) => {
     console.log(req.body)
     resp.send('<h1>Incluido!</h1>')
 })
+
+app.post('/usuarios/:id',(req,resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Usuário alterado!</h1>')
+})
 app.listen(3003)
 
 
